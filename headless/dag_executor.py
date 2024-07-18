d31ee058458afb198bfaa292c5cfd31c4924019d
Yashwanth Adunukota <yash@thirdai.com>
2024-07-18 13:14:23 -0500

    comments and readme for test suite

--- headless/dag_executor.py
+++ headless/dag_executor.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Callable, Dict, List, Optional
 
 import networkx as nx
 import yaml
@@ -7,13 +8,25 @@ logging.basicConfig(level=logging.INFO)
 
 
 class DAGExecutor:
-    def __init__(self, function_registry):
-        self.dags = {}
-        self.outputs = {}
-        self.variables = {}
-        self.function_registry = function_registry
+    def __init__(self, function_registry: Dict[str, Callable]):
+        """
+        Initializes the DAGExecutor with a function registry.
 
-    def load_dags_from_file(self, file_path):
+        Parameters:
+        function_registry (dict): A dictionary mapping function names to callable functions.
+        """
+        self.dags: Dict[str, nx.DiGraph] = {}
+        self.outputs: Dict[str, Any] = {}
+        self.variables: Dict[str, Any] = {}
+        self.function_registry: Dict[str, Callable] = function_registry
+
+    def load_dags_from_file(self, file_path: str):
+        """
+        Loads DAGs from a YAML configuration file.
+
+        Parameters:
+        file_path (str): Path to the YAML file containing DAG configurations.
+        """
         with open(file_path, "r") as file:
             config = yaml.safe_load(file)
 
@@ -34,18 +47,53 @@ class DAGExecutor:
                     task_info.get("params", {}),
                 )
 
-    def add_dag(self, dag_name):
+    def add_dag(self, dag_name: str):
+        """
+        Adds a new DAG to the executor.
+
+        Parameters:
+        dag_name (str): Name of the DAG to add.
+        """
         if dag_name not in self.dags:
             self.dags[dag_name] = nx.DiGraph()
 
-    def add_task(self, dag_name, task_name, func, dependencies=None, params=None):
+    def add_task(
+        self,
+        dag_name: str,
+        task_name: str,
+        func: Callable,
+        dependencies: Optional[List[str]] = None,
+        params: Optional[Dict[str, str]] = None,
+    ):
+        """
+        Adds a task to a specified DAG.
+
+        Parameters:
+        dag_name (str): Name of the DAG to add the task to.
+        task_name (str): Name of the task.
+        func (Callable): The function to execute for the task.
+        dependencies (list, optional): List of task names that this task depends on.
+        params (dict, optional): Dictionary of parameters for the task.
+        """
         self.add_dag(dag_name)
         self.dags[dag_name].add_node(task_name, func=func, params=params)
         if dependencies:
             for dep in dependencies:
                 self.dags[dag_name].add_edge(dep, task_name)
 
-    def get_execution_order(self, dag_name):
+    def get_execution_order(self, dag_name: str) -> List[str]:
+        """
+        Returns the execution order of tasks in a DAG.
+
+        Parameters:
+        dag_name (str): Name of the DAG.
+
+        Returns:
+        list: A list of task names in the order they should be executed.
+
+        Raises:
+        Exception: If the DAG has cycles, which are not allowed.
+        """
         graph = self.dags[dag_name]
         try:
             order = list(nx.topological_sort(graph))
@@ -53,13 +101,40 @@ class DAGExecutor:
         except nx.NetworkXUnfeasible:
             raise Exception(f"The DAG '{dag_name}' has cycles, which is not allowed.")
 
-    def get_task_func(self, dag_name, task_name):
+    def get_task_func(self, dag_name: str, task_name: str) -> Callable:
+        """
+        Retrieves the function associated with a task in a DAG.
+
+        Parameters:
+        dag_name (str): Name of the DAG.
+        task_name (str): Name of the task.
+
+        Returns:
+        Callable: The function associated with the task.
+        """
         return self.dags[dag_name].nodes[task_name]["func"]
 
-    def get_task_params(self, dag_name, task_name):
+    def get_task_params(self, dag_name: str, task_name: str) -> Dict[str, str]:
+        """
+        Retrieves the parameters associated with a task in a DAG.
+
+        Parameters:
+        dag_name (str): Name of the DAG.
+        task_name (str): Name of the task.
+
+        Returns:
+        dict: The parameters associated with the task.
+        """
         return self.dags[dag_name].nodes[task_name]["params"]
 
-    def execute_task(self, dag_name, task_name):
+    def execute_task(self, dag_name: str, task_name: str):
+        """
+        Executes a specified task in a DAG.
+
+        Parameters:
+        dag_name (str): Name of the DAG.
+        task_name (str): Name of the task.
+        """
         logging.info(f"Executing task '{task_name}' in DAG '{dag_name}'")
         task_func = self.get_task_func(dag_name, task_name)
         task_params = self.get_task_params(dag_name, task_name)
@@ -73,7 +148,13 @@ class DAGExecutor:
             self.outputs[task_name] = task_func(inputs)
         logging.info(f"Finished executing task '{task_name}' in DAG '{dag_name}'")
 
-    def execute_dag(self, dag_name):
+    def execute_dag(self, dag_name: str):
+        """
+        Executes all tasks in a specified DAG in the correct order.
+
+        Parameters:
+        dag_name (str): Name of the DAG.
+        """
         logging.info(f"Starting execution of DAG '{dag_name}'")
         order = self.get_execution_order(dag_name)
         for task_name in order:
@@ -81,13 +162,29 @@ class DAGExecutor:
         logging.info(f"Finished execution of DAG '{dag_name}'")
 
     def execute_all(self):
+        """
+        Executes all DAGs managed by the executor.
+        """
         for dag_name in self.dags:
             self.execute_dag(dag_name)
 
-    def set_variable(self, name, value):
+    def set_variable(self, name: str, value: Any):
+        """
+        Sets a variable to a specified value.
+
+        Parameters:
+        name (str): Name of the variable.
+        value (Any): Value to set for the variable.
+        """
         logging.info(f"Setting variable '{name}' to '{value}'")
         self.variables[name] = value
 
-    def update_variables(self, new_vars):
+    def update_variables(self, new_vars: Dict[str, Any]):
+        """
+        Updates multiple variables with new values.
+
+        Parameters:
+        new_vars (dict): A dictionary of new variable values.
+        """
         logging.info(f"Updating variables with '{new_vars}'")
         self.variables.update(new_vars)
--- headless/model.py
+++ headless/model.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 from thirdai.neural_db import ModelBazaar
 
@@ -8,26 +8,56 @@ from headless.utils import get_csv_source_id
 
 class Flow:
     def __init__(self, base_url: str, email: str, password: str):
+        """
+        Initializes the Flow object and logs into the ModelBazaar.
+
+        Parameters:
+        base_url (str): Base URL of the ModelBazaar API.
+        email (str): Email for authentication.
+        password (str): Password for authentication.
+        """
         self._bazaar_client = ModelBazaar(base_url=base_url)
         self._bazaar_client.log_in(email=email, password=password)
 
     @property
-    def bazaar_client(self):
+    def bazaar_client(self) -> ModelBazaar:
+        """
+        Returns the ModelBazaar client.
+
+        Returns:
+        ModelBazaar: The ModelBazaar client.
+        """
         return self._bazaar_client
 
     def train(
         self,
         model_name: str,
-        unsupervised_docs: List[str] = None,
-        supervised_docs: List[Tuple[str, str]] = None,
-        test_doc: str = None,
+        unsupervised_docs: Optional[List[str]] = None,
+        supervised_docs: Optional[List[Tuple[str, str]]] = None,
+        test_doc: Optional[str] = None,
         doc_type: str = "local",
-        extra_options: Dict = {},
-        base_model_identifier: str = None,
+        extra_options: Optional[Dict[str, str]] = {},
+        base_model_identifier: Optional[str] = None,
         is_async: bool = True,
-        metadata: List[Dict[str, str]] = None,
-        nfs_base_path: str = None,
+        metadata: Optional[List[Dict[str, str]]] = None,
+        nfs_base_path: Optional[str] = None,
     ):
+        """
+        Trains a model with the given documents and options.
+
+        Parameters:
+        model_name (str): Name of the model.
+        unsupervised_docs (list[str], optional): List of paths to unsupervised documents.
+        supervised_docs (list[tuple[str, str]], optional): List of tuples containing paths to supervised and unsupervised documents.
+        test_doc (str, optional): Path to the test document.
+        doc_type (str, optional): Type of documents (e.g., local, nfs).
+        extra_options (dict, optional): Additional training options.
+        base_model_identifier (str, optional): Identifier for the base model.
+        is_async (bool, optional): Whether the training should be asynchronous.
+        metadata (list[dict[str, str]], optional): Metadata for the documents.
+        nfs_base_path (str, optional): Base path for NFS storage.
+        """
+
         print("*" * 50 + f" Training the model: {model_name} " + "*" * 50)
         if supervised_docs:
             if metadata is None:
@@ -68,10 +98,18 @@ class Flow:
 
     def deploy(
         self,
-        model_identifier,
-        deployment_name,
+        model_identifier: str,
+        deployment_name: str,
         is_async: bool = True,
     ):
+        """
+        Deploys a model.
+
+        Parameters:
+        model_identifier (str): Identifier of the model to be deployed.
+        deployment_name (str): Name of the deployment.
+        is_async (bool, optional): Whether the deployment should be asynchronous.
+        """
         print("*" * 50 + f" Deploying the model {model_identifier} " + "*" * 50)
         return self._bazaar_client.deploy(
             model_identifier=model_identifier,

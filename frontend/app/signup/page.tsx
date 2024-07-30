be336e3bc3a73de3237916495b9359bbe434346c
Mr-Ye-Cao <yecaoyc2019@gmail.com>
2024-07-30 09:01:46 -0500

    sign up and log in

---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+import { signIn } from '@/lib/auth';
+import SignupForm from './signup-form';
+
+export default function SignupPage() {
+
+  return (
+    <div className="min-h-screen flex justify-center items-start md:items-center p-8">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign up</CardTitle>
+          <CardDescription>
+            Please fill in the details to create an account.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <SignupForm/>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- frontend/lib/backend.ts
+++ frontend/lib/backend.ts
@@ -1,6 +1,7 @@
+// /lib/backend.js
+
 import axios from 'axios';
 
-// /lib/backend.js
 export async function fetchPublicModels(name: string) {
     const response = await fetch(`http://localhost:8000/api/model/public-list?name=${name}`);
     if (!response.ok) {
@@ -15,6 +16,10 @@ interface TrainNdbParams {
 }
 
 export function train_ndb({ name, formData }: TrainNdbParams): Promise<any> {
+    const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTM4MGQ2NmQtMTIzZi00ODhkLTgwNTUtOTEwNGI5YzBhM2ViIiwiZXhwIjoxNzIyMzU0Mzk1fQ.Tl13wtCHnXes7nPxiI2KkHE4bJYqyXijNmOpjYDRQbA";
+
+    axios.defaults.headers.common.Authorization = `Bearer ${access_token}`;
+
     return new Promise((resolve, reject) => {
         axios
             .post(`http://localhost:8000/api/train/ndb?model_name=${name}&sharded=${false}`, formData)
@@ -28,5 +33,39 @@ export function train_ndb({ name, formData }: TrainNdbParams): Promise<any> {
                     reject(new Error('Failed to run model'));
                 }
             });
+    });
+}
+
+export function userEmailLogin(email: string, password: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get('http://localhost:8000/api/user/email-login', {
+          headers: {
+            Authorization: `Basic ${window.btoa(`${email}:${password}`)}`,
+          },
+        })
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+}
+
+export function userRegister(email: string, password: string, username: string) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post('http://localhost:8000/api/user/email-signup-basic', {
+          email,
+          password,
+          username,
+        })
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
 }

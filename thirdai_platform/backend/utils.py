2e0043077054b0f621701ab2153dcbc9c45e3ba7
Yash <yash@thirdai.com>
2024-07-30 13:44:59 -0500

    LLM generation job (#14)

--- thirdai_platform/backend/utils.py
+++ thirdai_platform/backend/utils.py
@@ -359,3 +359,27 @@ def delete_nomad_job(job_id, nomad_endpoint):
         )
 
     return response
+
+
+GENERATE_JOB_ID = "llm-generation"
+
+
+async def restart_generate_job():
+    nomad_endpoint = os.getenv("NOMAD_ENDPOINT")
+    if nomad_job_exists(GENERATE_JOB_ID, nomad_endpoint):
+        delete_nomad_job(GENERATE_JOB_ID, nomad_endpoint)
+    cwd = Path(os.getcwd())
+    platform = get_platform()
+    return submit_nomad_job(
+        nomad_endpoint=nomad_endpoint,
+        filepath=str(cwd / "backend" / "nomad_jobs" / "generation_job.hcl.j2"),
+        platform=platform,
+        port=None if platform == "docker" else get_empty_port(),
+        tag=os.getenv("TAG"),
+        registry=os.getenv("DOCKER_REGISTRY"),
+        docker_username=os.getenv("DOCKER_USERNAME"),
+        docker_password=os.getenv("DOCKER_PASSWORD"),
+        image_name=os.getenv("GENERATION_IMAGE_NAME"),
+        python_path=get_python_path(),
+        generate_app_dir=str(get_root_absolute_path() / "llm_generation_job"),
+    )

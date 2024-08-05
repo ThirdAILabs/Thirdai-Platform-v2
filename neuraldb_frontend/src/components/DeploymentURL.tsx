9a24799b7fc3a2b151b54c84aa257e44ffc4adc2
BenGeordie <school@bengeordie@gmail.com>
2024-08-05 17:07:37 -0400

    remove original ndb frontend

--- neuraldb_frontend/src/components/DeploymentURL.tsx
+++
@@ -1,6 +0,0 @@
-// ----------------------------------------------------------------------
-
-export function createDeploymentUrl(deploymentId: string) {
-    const hostname = `${window.location.protocol}//${window.location.host}`;
-    return `${hostname}/${deploymentId}`;
-}

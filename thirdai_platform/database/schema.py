bc52c733948eeb26c28a87661266d7dc8584cd36
Yashwanth Adunukota <yash@thirdai.com>
2024-08-05 15:14:46 -0500

    check

--- thirdai_platform/database/schema.py
+++ thirdai_platform/database/schema.py
@@ -58,6 +58,9 @@ class User(SQLDeclarativeBase):
         "Deployment", back_populates="user", cascade="all, delete-orphan"
     )
     logs = relationship("Log", back_populates="user", cascade="all, delete-orphan")
+    workflows = relationship(
+        "Workflow", back_populates="user", cascade="all, delete-orphan"
+    )
 
     @validates("username")
     def validate_username(self, key, username):
@@ -95,7 +98,7 @@ class Model(SQLDeclarativeBase):
 
     parent_deployment_id = Column(
         UUID(as_uuid=True),
-        ForeignKey("deployments.id", ondelete="SET NULL"),
+        ForeignKey("deployments.model_id", ondelete="SET NULL"),
         nullable=True,
     )  # Not null if this model comes from saving a deployment session
 
@@ -104,14 +107,6 @@ class Model(SQLDeclarativeBase):
     )
 
     user = relationship("User", back_populates="models")
-
-    parent_deployment = relationship(
-        "Deployment", back_populates="child_models", foreign_keys=[parent_deployment_id]
-    )
-
-    deployments = relationship(
-        "Deployment", back_populates="model", foreign_keys="[Deployment.model_id]"
-    )
 
     meta_data = relationship(
         "MetaData", back_populates="model", uselist=False, cascade="all, delete-orphan"
@@ -169,8 +164,10 @@ class ModelShard(SQLDeclarativeBase):
 class Deployment(SQLDeclarativeBase):
     __tablename__ = "deployments"
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()")
+    model_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("models.id", ondelete="CASCADE"),
+        primary_key=True,
     )
     name = Column(String(256), nullable=False)
     status = Column(ENUM(Status), nullable=False)
@@ -178,23 +175,8 @@ class Deployment(SQLDeclarativeBase):
     user_id = Column(
         UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
-    model_id = Column(
-        UUID(as_uuid=True),
-        ForeignKey("models.id", ondelete="CASCADE"),
-        nullable=False,
-    )
-
-    child_models = relationship(
-        "Model",
-        back_populates="parent_deployment",
-        foreign_keys=[Model.parent_deployment_id],
-    )
 
     user = relationship("User", back_populates="deployments")
-    model = relationship("Model", back_populates="deployments", foreign_keys=[model_id])
-    logs = relationship(
-        "Log", back_populates="deployment", cascade="all, delete-orphan"
-    )
 
     @validates("name")
     def validate_deployment_name(self, key, name):
@@ -210,9 +192,9 @@ class Deployment(SQLDeclarativeBase):
 class Log(SQLDeclarativeBase):
     __tablename__ = "logs"
 
-    deployment_id = Column(
+    model_id = Column(
         UUID(as_uuid=True),
-        ForeignKey("deployments.id", ondelete="CASCADE"),
+        ForeignKey("models.id", ondelete="CASCADE"),
         primary_key=True,
     )
     user_id = Column(
@@ -222,13 +204,54 @@ class Log(SQLDeclarativeBase):
     count = Column(Integer, nullable=False, default=0)
     log_entries = Column(JSON, nullable=True)
 
-    deployment = relationship("Deployment", back_populates="logs")
     user = relationship("User", back_populates="logs")
 
     __table_args__ = (
-        Index("log_deployment_index", "deployment_id"),
+        Index("log_model_index", "model_id"),
         Index("log_user_index", "user_id"),
         UniqueConstraint(
-            "deployment_id", "user_id", "action", name="unique_deployment_user_action"
+            "model_id", "user_id", "action", name="unique_model_user_action"
         ),
+    )
+
+
+class Workflow(SQLDeclarativeBase):
+    __tablename__ = "workflows"
+
+    id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()")
+    )
+    name = Column(String(256), nullable=False)
+    type = Column(String(256), nullable=False)
+    user_id = Column(
+        UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+
+    user = relationship("User", back_populates="workflows")
+    workflow_models = relationship(
+        "WorkflowModel", back_populates="workflow", cascade="all, delete-orphan"
+    )
+
+
+class WorkflowModel(SQLDeclarativeBase):
+    __tablename__ = "workflow_models"
+
+    workflow_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("workflows.id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    model_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("models.id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    train_status = Column(ENUM(Status), nullable=False, default=Status.not_started)
+    deployment_status = Column(ENUM(Status), nullable=True)
+
+    workflow = relationship("Workflow", back_populates="workflow_models")
+
+    __table_args__ = (
+        Index("workflow_model_index", "workflow_id"),
+        Index("model_workflow_index", "model_id"),
     )

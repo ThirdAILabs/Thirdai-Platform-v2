0853f49ef078f04bfe815c1337299fe6be80237d
benito <benito@thirdai.com>
2024-08-15 08:32:59 +0000

    Remove missing component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
-import UserWrapper from './user_wrapper';
 
 export const metadata = {
   title: 'ThirdAI Platform',
@@ -18,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex min-h-screen w-full flex-col bg-muted/40">
-        <UserWrapper>
-          {children}
-        </UserWrapper>
+        {children}
       </body>
       <Analytics />
     </html>

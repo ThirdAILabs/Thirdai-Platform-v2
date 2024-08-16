de0fd47c08ef20efb08391091ea0589499c71280
Yashwanth Adunukota <yash@thirdai.com>
2024-08-16 17:08:40 -0500

    some stale code

--- thirdai_platform/backend/utils.py
+++ thirdai_platform/backend/utils.py
@@ -641,32 +641,6 @@ async def restart_generate_job():
     )
 
 
-NEURALDB_DEPLOYMENT_ID = "neuraldb-deployment-ui"
-
-
-async def restart_neuraldb_deployment_ui():
-    nomad_endpoint = os.getenv("NOMAD_ENDPOINT")
-    if nomad_job_exists(NEURALDB_DEPLOYMENT_ID, nomad_endpoint):
-        delete_nomad_job(NEURALDB_DEPLOYMENT_ID, nomad_endpoint)
-    cwd = Path(os.getcwd())
-    return submit_nomad_job(
-        nomad_endpoint=nomad_endpoint,
-        filepath=str(
-            cwd / "backend" / "nomad_jobs" / "neuraldb_deployment_ui_job.hcl.j2"
-        ),
-        public_model_bazaar_endpoint=os.getenv("PRIVATE_MODEL_BAZAAR_ENDPOINT"),
-        platform=get_platform(),
-        tag=os.getenv("TAG"),
-        registry=os.getenv("DOCKER_REGISTRY"),
-        docker_username=os.getenv("DOCKER_USERNAME"),
-        docker_password=os.getenv("DOCKER_PASSWORD"),
-        image_name=os.getenv("SEARCH_IMAGE_NAME"),
-        # Model bazaar dockerfile does not include neuraldb_frontend code,
-        # but app_dir is only used if platform == local.
-        app_dir=str(get_root_absolute_path() / "neuraldb_frontend"),
-    )
-
-
 def get_expiry_min(size: int):
     """
     This is a helper function to calculate the expiry time for the signed

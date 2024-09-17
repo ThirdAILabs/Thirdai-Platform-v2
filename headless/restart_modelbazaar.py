0c246a7f6fa93239a73ceae9dc22754965958e73
Yashwanth Adunukota <yash@thirdai.com>
2024-09-17 15:44:55 -0500

    just see how the definition will get

--- headless/restart_modelbazaar.py
+++ headless/restart_modelbazaar.py
@@ -1,5 +1,5 @@
 # This to be run on same machine the nomad server is running.
-from headless.utils import fetch_job_definition, restart_nomad_job, stop_nomad_job
+from headless.utils import fetch_job_definition, restart_nomad_job, stop_nomad_job, update_docker_image_version
 
 if __name__ == "__main__":
     model_bazaar_job = "modelbazaar"
@@ -9,7 +9,13 @@ if __name__ == "__main__":
     definition = fetch_job_definition(
         job_id=model_bazaar_job, nomad_endpoint=nomad_endpoint
     )
+    
+    print(definition, flush=True)
+    
+    new_definition = update_docker_image_version(job_definition=definition, new_version="34.56.78.90")
+    
+    print(new_definition, flush=True)
 
-    stop_nomad_job(model_bazaar_job, nomad_endpoint=nomad_endpoint)
+    # stop_nomad_job(model_bazaar_job, nomad_endpoint=nomad_endpoint)
 
-    restart_nomad_job(nomad_endpoint, payload=definition)
+    # restart_nomad_job(nomad_endpoint, payload=definition)

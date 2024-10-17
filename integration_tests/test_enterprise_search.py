7ff50ec252fbee62a6968f0f6f91376b52bd61e0
shubh@3ai <shubh@thirdai.com>
2024-10-17 17:27:24 -0500

    arrrrrrrrrr

--- integration_tests/test_enterprise_search.py
+++ integration_tests/test_enterprise_search.py
@@ -46,8 +46,7 @@ def upload_guardrail_model(admin_client: ModelBazaar):
 
     # create a storage db for the model
     data_dir = Path(os.environ.get("SHARE_DIR", None)) / "data" / model_id
-    os.chmod(data_dir, 0o777)
-    os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(data_dir, exist_ok=True, mode=0o777)
 
     tag_metadata = TagMetadata(
         tag_status={"PHONENUMBER": LabelEntity(name="PHONENUMBER", status="trained")}
@@ -63,6 +62,7 @@ def upload_guardrail_model(admin_client: ModelBazaar):
     os.remove(path)
 
     return name, model_id
+
 
 @pytest.mark.unit
 def test_enterprise_search_with_guardrails():

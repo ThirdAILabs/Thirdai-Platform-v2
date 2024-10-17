3a0ded65ae3ba879f84005b84447da7ebf76c934
Pratik Pranav <pratik@thirdai.com>
2024-10-17 12:28:03 +0530

    small change

--- thirdai_platform/backend/startup_jobs.py
+++ thirdai_platform/backend/startup_jobs.py
@@ -124,6 +124,7 @@ async def restart_thirdai_platform_frontend():
             os.getenv("PUBLIC_MODEL_BAZAAR_ENDPOINT")
         ),
         use_ssl_in_login=os.getenv("USE_SSL_IN_LOGIN").lower(),
+        share_dir=os.getenv("SHARE_DIR"),
         # Model bazaar dockerfile does not include neuraldb_frontend code,
         # but app_dir is only used if platform == local.
         app_dir=str(get_root_absolute_path() / "frontend"),

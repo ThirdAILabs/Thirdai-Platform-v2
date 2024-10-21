473e70876879b6086a3ac6eeeb37bc2977dd7e6d
Gautam Sharma <gautam@thirdai.com>
2024-10-21 11:55:42 +0530

    minor changes

--- thirdai_platform/backend/routers/deploy.py
+++ thirdai_platform/backend/routers/deploy.py
@@ -423,7 +423,7 @@ def get_feedback(
         str(model.id),
         "deployments",
         "data",
-        "collected_feedbacks",
+        "recent_feedbacks",
     )
 
     if not os.path.exists(feedback_dir) or len(os.listdir(feedback_dir)) == 0:

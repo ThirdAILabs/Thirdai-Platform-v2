4e00fef6cfaca89eb126160ec439d6e7e16ee7f5
Nicholas Meisburger <nmeisburger@gmail.com>
2024-10-23 17:57:33 -0400

    maximum size for requests

--- thirdai_platform/deployment_job/routers/ndb.py
+++ thirdai_platform/deployment_job/routers/ndb.py
@@ -210,6 +210,13 @@ class NDBRouter:
                 message="No documents supplied for insertion. Must supply at least one document.",
             )
 
+        total_filesize = sum(file.size or 0 for file in files)
+        if total_filesize > 10 * 1024 * 1024:
+            return response(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                message=f"Size of uploaded files exceeds maximum of 10Mb for insertion endpoint on active deployment. Please use retraining api for updates of this size.",
+            )
+
         documents = download_local_files(
             files=files,
             file_infos=documents,
@@ -264,6 +271,11 @@ class NDBRouter:
                 message="Delete logged successfully.",
             )
         else:
+            if len(input.source_ids) > 5:
+                return response(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    message="Number of deletions exceeds the maximum that can be processed synchronously in an active deployment.",
+                )
             self.model.delete(input.source_ids)
 
             return response(
@@ -318,6 +330,11 @@ class NDBRouter:
                 message="Upvote logged successfully.",
             )
         else:
+            if len(input.text_id_pairs) > 100:
+                return response(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    message="Number of deletions exceeds the maximum that can be processed synchronously in an active deployment.",
+                )
             self.model.upvote(input.text_id_pairs)
 
             return response(
@@ -370,6 +387,11 @@ class NDBRouter:
                 message="Associate logged successfully.",
             )
         else:
+            if len(input.text_pairs) > 100:
+                return response(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    message="Number of deletions exceeds the maximum that can be processed synchronously in an active deployment.",
+                )
             self.model.associate(input.text_pairs)
 
             return response(

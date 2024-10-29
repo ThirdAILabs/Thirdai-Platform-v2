9b123d216022705bae74ba3a3a1dadaaead6a018
Yashwanth Adunukota <yash@thirdai.com>
2024-10-28 20:02:25 -0500

    cache job done

--- thirdai_platform/llm_cache_job/main.py
+++ thirdai_platform/llm_cache_job/main.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import sys
+from pathlib import Path
 
 import thirdai
 from fastapi import APIRouter, Depends, FastAPI, status
@@ -8,6 +10,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from llm_cache_job.cache import Cache, NDBSemanticCache
 from llm_cache_job.permissions import Permissions
+from platform_common.logging import LoggerConfig, StreamToLogger
+from platform_common.middlewares import create_log_request_response_middleware
 
 app = FastAPI()
 router = APIRouter()
@@ -29,21 +33,28 @@ if license_key == "file_license":
 else:
     thirdai.licensing.activate(license_key)
 
+log_dir: Path = Path(model_bazaar_dir) / "logs"
+
+log_dir.mkdir(parents=True, exist_ok=True)
+
+logger_file_path = log_dir / "llm_cache.log"
+logger = LoggerConfig(logger_file_path).get_logger("llm-cache-logger")
+
+sys.stdout = StreamToLogger(logger, logging.INFO, sys.stdout)
+sys.stderr = StreamToLogger(logger, logging.ERROR, sys.stderr)
+
 permissions = Permissions()
 
-cache: Cache = NDBSemanticCache()
+cache: Cache = NDBSemanticCache(logger=logger)
 
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+app.add_middleware(create_log_request_response_middleware(logger))
 
 
 @router.get("/suggestions", dependencies=[Depends(permissions.verify_read_permission)])
 def suggestions(model_id: str, query: str):
     result = cache.suggestions(model_id=model_id, query=query)
 
-    logging.info(f"found {len(result)} suggestions for query={query}")
+    logger.info(f"found {len(result)} suggestions for query={query}")
 
     return JSONResponse(
         status_code=status.HTTP_200_OK,
@@ -56,9 +67,9 @@ def cache_query(model_id: str, query: str):
     result = cache.query(model_id=model_id, query=query)
 
     if result:
-        logging.info(f"found cached result for query={query}")
+        logger.info(f"found cached result for query={query}")
     else:
-        logging.info(f"found no cached result for query={query}")
+        logger.info(f"found no cached result for query={query}")
 
     return JSONResponse(
         status_code=status.HTTP_200_OK,
@@ -74,7 +85,7 @@ def cache_insert(
 ):
     cache.insert(model_id=model_id, query=query, llm_res=llm_res)
 
-    logging.info(f"cached query={query}")
+    logger.info(f"cached query={query}")
 
     return JSONResponse(
         status_code=status.HTTP_200_OK,
@@ -94,7 +105,7 @@ def cache_invalidate(model_id: str):
 
 @router.get("/token", dependencies=[Depends(permissions.verify_read_permission)])
 def temporary_cache_token(model_id: str):
-    logging.info(f"creating cache token for model {model_id}")
+    logger.info(f"creating cache token for model {model_id}")
 
     return JSONResponse(
         status_code=status.HTTP_200_OK,

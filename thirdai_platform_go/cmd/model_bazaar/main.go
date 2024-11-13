59529a93a06b3a0dff003bbf3bd2575fc054f83b
Nicholas Meisburger <nmeisburger@gmail.com>
2024-11-12 19:21:16 -0500

    parse config as json

--- thirdai_platform_go/cmd/model_bazaar/main.go
+++ thirdai_platform_go/cmd/model_bazaar/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -48,6 +50,24 @@ func (c *Config) Driver() nomad.Driver {
 	panic("either LocalDriver or DockerDriver must be specified in config")
 }
 
+func loadConfig() Config {
+	configFilePath := flag.String("config", "", "the config file to use to start model_bazaar")
+	flag.Parse()
+
+	configFile, err := os.Open(*configFilePath)
+	if err != nil {
+		log.Fatalf("unable to open config file '%v': %v", configFilePath, err)
+	}
+
+	var c Config
+	err = json.NewDecoder(configFile).Decode(&c)
+	if err != nil {
+		log.Fatalf("unable to parse config file: %v", err)
+	}
+
+	return c
+}
+
 func initLogging(logFile *os.File) {
 	log.SetFlags(log.Lshortfile | log.Ltime | log.Ldate)
 	log.SetOutput(io.MultiWriter(logFile, os.Stderr))
@@ -57,7 +77,7 @@ func initLogging(logFile *os.File) {
 func initDb(dsn string) *gorm.DB {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Panicf("error opening database connection: %v", err)
+		log.Fatalf("error opening database connection: %v", err)
 	}
 
 	err = db.AutoMigrate(
@@ -65,18 +85,18 @@ func initDb(dsn string) *gorm.DB {
 		&schema.User{}, &schema.Team{}, &schema.UserTeam{},
 	)
 	if err != nil {
-		log.Panicf("error migrating db schema: %v", err)
+		log.Fatalf("error migrating db schema: %v", err)
 	}
 
 	return db
 }
 
 func main() {
-	var c Config
+	c := loadConfig()
 
 	logFile, err := os.OpenFile(filepath.Join(c.ShareDir, "logs/model_bazaar.log"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 	if err != nil {
-		log.Panicf("error opening log file: %v", err)
+		log.Fatalf("error opening log file: %v", err)
 	}
 	defer logFile.Close()
 

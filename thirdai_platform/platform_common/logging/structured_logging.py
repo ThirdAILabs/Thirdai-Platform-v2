7acb6a764e594e47196866bd4c0e75464bb8c142
shubh@3ai <shubh@thirdai.com>
2024-11-21 16:45:21 -0600

    fix base logger

--- thirdai_platform/platform_common/logging/structured_logging.py
+++ thirdai_platform/platform_common/logging/structured_logging.py
@@ -2,8 +2,10 @@ import json
 import logging
 from datetime import datetime
 from pathlib import Path
+from typing import Dict, Union
 
 from colorlog import ColoredFormatter
+from platform_common.logging.logcodes import LogCode
 
 
 class JSONFormatter(logging.Formatter):
@@ -15,15 +17,16 @@ class JSONFormatter(logging.Formatter):
 
         # Basic log entry with required fields
         log_entry = {
-            "timestamp": timestamp,
             "level": record.levelname,
-            "logger_name": record.name,
-            "message": str(record.msg),
+            "service_type": record.name,
+            # reserved fields name for VictoriaLogs
+            "_time": timestamp,
+            "_msg": str(record.msg),
         }
 
         # Add code if present
-        if hasattr(record, "log_code"):
-            log_entry["code"] = record.log_code
+        if hasattr(record, "code"):
+            log_entry["code"] = record.code
 
         # Add extra fields if present
         if hasattr(record, "extra_fields"):
@@ -33,24 +36,29 @@ class JSONFormatter(logging.Formatter):
 
 
 class BaseLogger:
-    """Abstract base class for all loggers"""
+    """Base class for all loggers"""
 
     def __init__(
         self,
         log_dir: Path,
         log_prefix: str,
-        service_name: str = "default",
+        service_type: str,
         level=logging.INFO,
     ):
-        self.logger = self._setup_logger(log_dir, log_prefix, service_name, level)
+        self.logger = self._setup_logger(log_dir, log_prefix, service_type, level)
+
+    @property
+    def get_logger_keys(self) -> Dict[str, Union[str, int]]:
+        """Returns fields that can be used to uniquely identify a log stream."""
+        return {}
 
     def _setup_logger(
-        self, log_dir: Path, log_prefix: str, service_name: str, level: int
+        self, log_dir: Path, log_prefix: str, service_type: str, level: int
     ) -> logging.Logger:
         log_dir.mkdir(parents=True, exist_ok=True)
         logger_file_path = log_dir / f"{log_prefix}.log"
 
-        logger = logging.getLogger(service_name)
+        logger = logging.getLogger(service_type)
         logger.setLevel(level)
         logger.handlers = []
 
@@ -79,3 +87,33 @@ class BaseLogger:
         """Basic logging method that only takes code, message and extra fields"""
         extra = {"code": code, "extra_fields": extra_fields}
         self.logger.log(level, message, extra=extra)
+
+    def info(self, code: LogCode, message: str, **extra_fields):
+        """Log an info message with code and extra fields"""
+        logger_keys = self.get_logger_keys
+        extra_fields.update(logger_keys)
+        self._log(logging.INFO, code, message, **extra_fields)
+
+    def debug(self, code: LogCode, message: str, **extra_fields):
+        """Log a debug message with code and extra fields"""
+        logger_keys = self.get_logger_keys
+        extra_fields.update(logger_keys)
+        self._log(logging.DEBUG, code, message, **extra_fields)
+
+    def warning(self, code: LogCode, message: str, **extra_fields):
+        """Log a warning message with code and extra fields"""
+        logger_keys = self.get_logger_keys
+        extra_fields.update(logger_keys)
+        self._log(logging.WARNING, code, message, **extra_fields)
+
+    def error(self, code: LogCode, message: str, **extra_fields):
+        """Log an error message with code and extra fields"""
+        logger_keys = self.get_logger_keys
+        extra_fields.update(logger_keys)
+        self._log(logging.ERROR, code, message, **extra_fields)
+
+    def critical(self, code: LogCode, message: str, **extra_fields):
+        """Log a critical message with code and extra fields"""
+        logger_keys = self.get_logger_keys
+        extra_fields.update(logger_keys)
+        self._log(logging.CRITICAL, code, message, **extra_fields)

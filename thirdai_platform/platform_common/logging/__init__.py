3eeb59511e3fc57c47482d5acc959adfb3836dfb
shubh@3ai <shubh@thirdai.com>
2024-11-22 09:21:39 -0600

    change logging dir

--- thirdai_platform/platform_common/logging/__init__.py
+++ thirdai_platform/platform_common/logging/__init__.py
@@ -1,5 +1,13 @@
 from platform_common.logging.base_logger import BaseLogger
+from platform_common.logging.job_loggers import DeploymentLogger, TrainingLogger
 from platform_common.logging.logcodes import LogCode
-from platform_common.logging.loggers import DeploymentLogger, TrainingLogger
+from platform_common.logging.logging import get_default_logger, setup_logger
 
-__all__ = ["BaseLogger", "LogCode", "TrainingLogger", "DeploymentLogger"]
+__all__ = [
+    "BaseLogger",
+    "LogCode",
+    "TrainingLogger",
+    "DeploymentLogger",
+    "get_default_logger",
+    "setup_logger",
+]

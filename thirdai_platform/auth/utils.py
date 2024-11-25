4f89288eea93211833ee64985a6e7cfc511f14e6
Pratik Pranav <pratik@thirdai.com>
2024-11-25 22:05:08 +0530

    update comments

--- thirdai_platform/auth/utils.py
+++ thirdai_platform/auth/utils.py
@@ -239,7 +239,7 @@ if IDENTITY_PROVIDER == "keycloak":
         realm_representation["accountTheme"] = "custom-theme"
         realm_representation["adminTheme"] = "custom-theme"
         realm_representation["emailTheme"] = "custom-theme"
-        realm_representation["displayNameHtml"] = " "  # for showing up nothing
+        realm_representation["displayNameHtml"] = " "  # For showing up nothing
 
         keycloak_admin.update_realm(new_realm_name, realm_representation)
 

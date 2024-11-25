66d959ab15358c24cdc3d754cb9e9a705d786507
Gautam Sharma <gautam@thirdai.com>
2024-11-25 17:05:17 +0530

    minor changes

--- thirdai_platform/platform_common/file_handler.py
+++ thirdai_platform/platform_common/file_handler.py
@@ -721,7 +721,7 @@ def get_cloud_client(provider: str):
             f"Currently supports s3,azure and gcp, but received {provider}"
         )
 
-
+# TODO (anyone): pass in the logger object to log at appropriate file.
 def download_file(doc: FileInfo, tmp_dir: str):
     """
     General method to download a file from S3, Azure, or GCP to a temporary directory.

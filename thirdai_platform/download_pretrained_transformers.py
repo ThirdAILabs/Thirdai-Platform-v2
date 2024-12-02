098bb88d9f3b6ab56010aaa354f272b222167f6a
Kartik Sarangmath <kartik@thirdai.com>
2024-12-02 14:26:49 -0600

    comment

--- thirdai_platform/download_pretrained_transformers.py
+++ thirdai_platform/download_pretrained_transformers.py
@@ -1,5 +1,8 @@
 from transformers import AutoModelForMaskedLM, AutoModelForSequenceClassification
 
+# The following models are downloaded in Universe. If we change the models we use in Universe,
+# we must also change the models that we load here.
+
 model = AutoModelForSequenceClassification.from_pretrained(
     "jinaai/jina-reranker-v1-tiny-en", num_labels=1, trust_remote_code=True
 )

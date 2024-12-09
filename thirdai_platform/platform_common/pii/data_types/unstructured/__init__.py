ef959248915110fda62ae19ba222074421579583
shubh@3ai <shubh@thirdai.com>
2024-12-09 16:59:38 -0600

    format

--- thirdai_platform/platform_common/pii/data_types/unstructured/__init__.py
+++ thirdai_platform/platform_common/pii/data_types/unstructured/__init__.py
@@ -1,5 +1,3 @@
-from platform_common.pii.data_types.unstructured.impl import (
-    UnstructuredText,
-)
+from platform_common.pii.data_types.unstructured.impl import UnstructuredText
 
 __all__ = ["UnstructuredText"]

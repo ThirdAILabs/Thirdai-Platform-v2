07f7f68aa2c8f53e1fb9180cfa836cc3c3a22fe7
Nicholas Meisburger <nmeisburger@gmail.com>
2025-01-17 13:05:14 -0500

    add example webserver

---
+++ thirdai_platform/cmd/ndb_server/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"thirdai_platform/ndb"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type NDBServer struct {
+	ndb ndb.NeuralDB
+}
+
+type searchRequest struct {
+	Query string `json:"query"`
+	Topk  int    `json:"top_k"`
+}
+
+type searchResult struct {
+	Id     int     `json:"id"`
+	Text   string  `json:"text"`
+	Source string  `json:"source"`
+	Score  float32 `json:"score"`
+}
+
+type searchResults struct {
+	References []searchResult `json:"references"`
+}
+
+func (s *NDBServer) Search(w http.ResponseWriter, r *http.Request) {
+	var req searchRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, fmt.Sprintf("request parsing error: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	chunks, err := s.ndb.Query(req.Query, req.Topk)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("ndb query error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	results := searchResults{References: make([]searchResult, 0, len(chunks))}
+	for _, chunk := range chunks {
+		results.References = append(results.References, searchResult{
+			Id:     int(chunk.Id),
+			Text:   chunk.Text,
+			Source: chunk.Document,
+			Score:  chunk.Score,
+		})
+	}
+
+	if err := json.NewEncoder(w).Encode(&results); err != nil {
+		http.Error(w, fmt.Sprintf("error writing request: %v", err), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	var ndbPath string
+	flag.StringVar(&ndbPath, "ndb_path", "", "the path to load the neural db from")
+	flag.Parse()
+
+	ndb, err := ndb.New(ndbPath)
+	if err != nil {
+		log.Fatalf("failed to open ndb: %v", err)
+	}
+	defer ndb.Free()
+
+	server := NDBServer{ndb: ndb}
+
+	r := chi.NewRouter()
+	r.Post("/search", server.Search)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
--- thirdai_platform/cmd/ndb_test/main.go
+++ thirdai_platform/cmd/ndb_test/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"thirdai_platform/ndb"
+	"time"
 )
 
-func main() {
+func basicTest() {
 	ndb, err := ndb.New("./tmp2.db")
 	if err != nil {
 		log.Fatalf("failed to open ndb: %v", err)
@@ -24,4 +28,63 @@ func main() {
 	}
 
 	fmt.Println(res)
+}
+
+type sample struct {
+	label uint64
+	query string
+}
+
+func loadQueries(filename string) []sample {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	rows, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalf("failed to read csv: %v", err)
+	}
+
+	samples := make([]sample, 0)
+	for _, row := range rows[1:] {
+		label, err := strconv.ParseUint(row[0], 10, 64)
+		if err != nil {
+			log.Fatalf("failed to parse label: %v", err)
+		}
+		samples = append(samples, sample{label: label, query: row[1]})
+	}
+
+	return samples
+}
+
+func main() {
+	ndb, err := ndb.New("../wiki_large.ndb")
+	if err != nil {
+		log.Fatalf("failed to open ndb: %v", err)
+	}
+	defer ndb.Free()
+
+	samples := loadQueries("../../data/semantic_benchmarks/wiki/len_10.csv")
+
+	correct := 0
+	start := time.Now()
+
+	for _, sample := range samples {
+		results, err := ndb.Query(sample.query, 5)
+		if err != nil {
+			log.Fatalf("ndb query failed: %v", err)
+		}
+
+		if len(results) > 0 && results[0].Id == sample.label {
+			correct++
+		}
+	}
+
+	end := time.Now()
+
+	fmt.Printf("p@1=%f\n", float64(correct)/float64(len(samples)))
+	fmt.Printf("avg_time=%f\n", float64(end.Sub(start).Milliseconds())/float64(len(samples)))
 }

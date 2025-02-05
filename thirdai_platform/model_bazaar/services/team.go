223a275437c09c866c0dfe6791e196aff23a10d1
Nicholas Meisburger <nmeisburger@gmail.com>
2025-02-05 09:51:20 -0500

    check user is in team

--- thirdai_platform/model_bazaar/services/team.go
+++ thirdai_platform/model_bazaar/services/team.go
@@ -203,6 +203,10 @@ func (s *TeamService) RemoveUserFromTeam(w http.ResponseWriter, r *http.Request)
 			return err
 		}
 
+		if err := checkUserTeamExists(txn, userId, teamId); err != nil {
+			return err
+		}
+
 		result := txn.Delete(&schema.UserTeam{UserId: userId, TeamId: teamId})
 		if result.Error != nil {
 			slog.Error("sql error deleting user_team entry", "team_id", teamId, "user_id", userId, "error", result.Error)
@@ -368,6 +372,10 @@ func (s *TeamService) RemoveTeamAdmin(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if err := checkUserExists(txn, userId); err != nil {
+			return err
+		}
+
+		if err := checkUserTeamExists(txn, userId, teamId); err != nil {
 			return err
 		}
 

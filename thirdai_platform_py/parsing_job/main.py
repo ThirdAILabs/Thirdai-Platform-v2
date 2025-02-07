e13fa612b3ff91994337a0cd2b5aa651d4a45be7
dtr3 <david@thirdai.com>
2025-02-07 23:48:04 +0000

    permissions comment

--- thirdai_platform_py/parsing_job/main.py
+++ thirdai_platform_py/parsing_job/main.py
@@ -81,6 +81,10 @@ class ParseRequest(BaseModel):
     options: Dict[str, Any] = {}
 
 
+# TODO add permissions. What's the best way to do service-to-service communication?
+# We could ping the permissions endpoint but we'd be calling this function once 
+# per doc. Better would be if this endpoint is not accessible via external calls 
+# and only accessible via internal calls, without JWT authentication
 @app.post("/parse")
 def parse_doc(req: ParseRequest):
     file_path = os.path.join(model_bazaar_dir, "uploads", req.upload_id, req.filename)

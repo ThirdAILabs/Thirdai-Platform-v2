a4ecd547d863b9cf673d0582190fc744b6025df5
BenGeordie <school@bengeordie@gmail.com>
2025-04-24 13:29:16 -0500

    save button

--- frontend/app/semantic-search/[deploymentId]/components/buttons/SaveButton.tsx
+++ frontend/app/semantic-search/[deploymentId]/components/buttons/SaveButton.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { FaSave } from 'react-icons/fa';
 import { Button } from '@mui/material';
 
-export default function SaveButton(props: { onClick: () => void }) {
+interface SaveButtonProps {
+  onClick: () => void;
+  style?: React.CSSProperties;
+}
+
+export default function SaveButton({ onClick, style }: SaveButtonProps) {
+  const defaultStyle = {
+    width: '48px',
+    height: '48px',
+    minWidth: 'unset',
+    padding: '12px',
+    ...style
+  };
+
   return (
     <Button
       variant="contained"
       color="primary"
-      style={{
-        width: '48px',
-        height: '48px',
-        minWidth: 'unset',
-        padding: '12px',
-      }}
-      onClick={props.onClick}
+      style={defaultStyle}
+      onClick={onClick}
     >
       <FaSave style={{ fontSize: '24px' }} />
     </Button>
